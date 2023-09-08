a044b29984bf930e6214889a5dcfcb9d6dd6435b
leonrdo.bozzi <bozzileonardo8@gmail.com>
2023-09-08 17:49:56 -0300

    :sparkles: date compare

--- src/components/NextRaces/index.tsx
+++ src/components/NextRaces/index.tsx
@@ -11,9 +11,6 @@ export default function NextRaces() {
 
     const actualDate = new Date();
 
-    const month = String(actualDate.getMonth() + 1).padStart(2, "0"); // O mês é baseado em zero, então adicionamos 1 e formatamos para 2 dígitos
-    const day = String(actualDate.getDate()).padStart(2, "0");
-
     const nextRaceFilter = data.MRData.RaceTable.Races.filter((item: any) => {
       const itemDate = new Date(
         item.date.split("-")[0] +
